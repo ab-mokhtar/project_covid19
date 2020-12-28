68456a62ac2781cdd65e7330fa2ac9d8c2d50788
Abbes <mokhtarabbes9@gmail.com>
2020-12-28 05:47:29 +0100

    fragment

---
+++ app/src/main/java/com/dsi31g1/covid19/List_hosp_fragment.java
@@ -0,0 +1,110 @@
+package com.dsi31g1.covid19;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link List_hosp_fragment#newInstance} factory method to
+ * create an instance of this fragment.
+ *
+ */
+public class List_hosp_fragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment List_hosp_fragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static List_hosp_fragment newInstance(String param1, String param2) {
+        List_hosp_fragment fragment = new List_hosp_fragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public List_hosp_fragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View RootView = inflater.inflate(R.layout.fragment_list_hosp_fragment, container, false);
+        final ArrayList<HashMap<String,String>> list= new ArrayList();
+        HashMap<String,String>Log= new HashMap<String,String>();
+        Log.put("Nom","hbib thamer");
+        Log.put("lieu"," rue hbib thamer tunis");
+        Log.put("nblit","50/50");
+        Log.put("phone","71380216");
+        list.add(Log);
+        String[]from={"Nom","lieu","nblit"};
+        int[] to= { R.id.titreview, R.id.descr,R.id.nb_lit};
+        //LogAdapter logAdapter = new LogAdapter (this, Log, LogImg);
+        final ListView list1 = RootView.findViewById(R.id.ListeView1);
+        SimpleAdapter adapter = new SimpleAdapter(getActivity().getBaseContext(),list,R.layout.ligne, from, to);
+        list1.setAdapter(adapter);
+        list1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                Toast.makeText(getContext(),list.get(position).get("title"),Toast.LENGTH_LONG).show();
+            }
+
+        });
+        list1.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener(){
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setTitle("Sélection Item");
+                builder.setMessage("Hôspital: "+list.get(position).get("Nom")+" Adresse: "+" "+list.get(position).get("lieu")+" nombre de lite occupé: "+list.get(position).get("nblit")+" Tel:"+list.get(position).get("phone"));
+                builder.setCancelable(true);
+                builder.setPositiveButton("ok", null).show();
+
+                return true;
+            }
+        });
+
+        // Inflate the layout for this fragment
+        return RootView;
+
+
+    }
+}
--- app/src/main/java/com/dsi31g1/covid19/MainActivity.java
+++ app/src/main/java/com/dsi31g1/covid19/MainActivity.java
@@ -1,6 +1,7 @@
 package com.dsi31g1.covid19;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -16,24 +17,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
-        bottomNavigationView.setSelectedItemId(R.id.home);
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                int itemId = menuItem.getItemId();
-                if (itemId == R.id.about) {
-                    MainActivity mainActivity = MainActivity.this;
-                    mainActivity.startActivity(new Intent(mainActivity.getApplicationContext(), About.class));
-                    MainActivity.this.overridePendingTransition(0, 0);
-                    return true;
-                } else if (itemId != R.id.stat) {
-                    return itemId == R.id.home;
-                } else {
-                    MainActivity mainActivity2 = MainActivity.this;
-                    mainActivity2.startActivity(new Intent(mainActivity2.getApplicationContext(), Stat.class));
-                    MainActivity.this.overridePendingTransition(0, 0);
-                    return true;
-                }
-            }
-        });
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
+
     }
-}
+        private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedfragment  = null;
+
+                switch (item.getItemId()){
+                    case R.id.home:
+                        selectedfragment = new home();
+                        break;
+                    case R.id.stat:
+                        selectedfragment = new stat_fragment();
+                        break;
+                    case R.id.hospital:
+                        selectedfragment=new List_hosp_fragment();
+                        break;
+                    case R.id.about:
+                        selectedfragment=new about_fragment();
+                        break;
+
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment,selectedfragment).commit();
+                return true;
+            }
+
+        };
+        }
